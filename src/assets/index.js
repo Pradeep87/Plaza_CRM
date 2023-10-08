65b76a36fe210381933e610418aa2287fddfafa5
Pradeep87 <pradeep87055@gmail.com>
2023-10-08 13:45:10 +0530

    section three added

--- src/assets/index.js
+++ src/assets/index.js
@@ -2,9 +2,17 @@ import whiteLogo from './images/Logo-White.png'
 import Login from './images/Login.png'
 import RegisterIcon from './images/Register-Icon.png'
 import SliderImage from './images/Slider-Image.png'
+import Illustration from './images/Trial-Illustration.png'
+import FinanceSolution from './images/Finance-Solution.png'
+import BusinessAnalysis from './images/Business-Analysis.png'
+import MarketingStrategy from './images/Marketing-Strategy.png'
+import CustomerService from './images/Customer-Service.png'
+import Sales from './images/Sales.png'
 
 export {
     whiteLogo,
-    Login,RegisterIcon,SliderImage
+    Login, RegisterIcon, SliderImage, Illustration,
+     FinanceSolution, BusinessAnalysis, MarketingStrategy,
+      CustomerService, Sales
 }
 
--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,6 +1,59 @@
 import React from 'react'
 import { Container, ContentWrapper } from '../../layouts';
-import { SliderImage } from '../../assets';
+import {
+  SliderImage, Illustration, FinanceSolution, BusinessAnalysis, MarketingStrategy,
+  CustomerService, Sales
+} from '../../assets';
+import { Link } from 'react-router-dom';
+
+
+const solutions = [
+  {
+    id:1,
+    icon: FinanceSolution,
+    heading: "Finance Solutions",
+    text: "Finance is the management of money, particularly in relation to companies, organisations, or"
+  },
+  {
+    id:2,
+    icon: BusinessAnalysis,
+    heading: "Business Analysis",
+    text: "Business analytics refers to the skills, technologies, practices for continuous iterative exploration"
+  },
+  {
+    id:2,
+    icon: MarketingStrategy,
+    heading: "Marketing Strategy",
+    text: "A market analysis studies the attractiveness and the dynamics of a special market ..."
+  },
+  {
+    id:4,
+    icon: CustomerService,
+    heading: "Customer Service",
+    text: "Customer service is the provision of service to customers before, during, and after a purchase"
+  },
+  {
+    id:5,
+    icon: Sales,
+    heading: "Sales",
+    text: "generating sales requires potential customers to understand that a product exists,"
+  },
+]
+
+
+
+const Solutions = ({ id, icon, heading, text }) => {
+  return (
+    <div className="solutionsCard">
+      <img src={icon} alt={heading} />
+      <p className='heading'>{heading}</p>
+      <p className='text'>{text}</p>
+      <Link to={`/solutions/${id}`}>Read More</Link>
+    </div>
+  )
+}
+
+
 
 const SectionOne = () => <Container className={'sectionOne'}>
   <ContentWrapper className={'content'} >
@@ -18,10 +71,51 @@ const SectionOne = () => <Container className={'sectionOne'}>
 </Container>
 
 
+const SectionTwo = () => <Container className={'sectionTwo'}>
+  <ContentWrapper className={'content'} >
+    <div className='image'>
+      <img src={Illustration} alt="Illustration" />
+    </div>
+    <div className='registerForm'>
+      <p className='heading'>Get started with your <br /> <span>14-day free trial</span></p>
+      <form action="" >
+        <input type="text" name='companyName' placeholder='Company Name' />
+        <input type="text" name='email' placeholder='Email' />
+        <input type="text" name='phoneNumber' placeholder='Phone Number' />
+        <input type="password" name='password' placeholder='Password' />
+        <input type="password" name='confirmPassword' placeholder='Confirm Password' />
+        <div className='conditions'>
+          <input type="checkbox" name="" id="" />
+          <p className='text'>
+            Agree with <span>Term & Conditons</span></p>
+        </div>
+
+        <button type="submit">Create Account</button>
+      </form>
+    </div>
+  </ContentWrapper>
+</Container>
+
+
+const SectionThree = () => <Container className={'sectionThree'}>
+  <ContentWrapper >
+    <p className='heading'>Solutions</p>
+    <p className='subtext'>One Application solver your all Digital Problems</p>
+
+    <div className='solutions'>
+      {solutions.map((data, idx) => <Solutions key={idx} {...data} />)
+      }
+    </div>
+  </ContentWrapper>
+</Container>
+
+
 const Home = () => {
   return (
     <>
       <SectionOne />
+      <SectionTwo />
+      <SectionThree />
     </>
   )
 }
