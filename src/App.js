34d4915d273a19713caeeecdf3629f20311e2e6e
Pradeep87 <pradeep87055@gmail.com>
2023-10-08 10:39:06 +0530

    pages added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import React from 'react'
+import { Routes, Route } from 'react-router-dom'
+import { Home, Features, NotFound } from './pages'
 
 const App = () => {
   return (
-    <div>
-    <h1>app</h1>
-    </div>
+    <Routes>
+      <Route path='/' element={<Home />} />
+      <Route path='/features' element={<Features />} />
+      <Route path='*' element={<NotFound />} />
+    </Routes>
   )
 }
 
---
+++ src/pages/index.js
@@ -0,0 +1,3 @@
+export {default as Home } from './home/Home'
+export {default as Features } from './features/Features'
+export {default as NotFound } from './notFound/NotFound'
